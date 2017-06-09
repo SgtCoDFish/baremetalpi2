2b443bce8e7dbb4adfc4cccc8535d9e196e49454
Ashley Davis <ashley_davis10419@hotmail.com>
2017-06-09 01:32:07 +0100

    add pizero ledclear

---
+++ ledclear/pizero/ledclear.s
@@ -0,0 +1,33 @@
+.globl _start
+
+_start:
+	ldr r0, .reg
+	ldr r1, [r0]
+
+	bic r1, #0x00e00000
+	orr r1, #0x00200000
+
+	str r1, [r0]
+	sub r0, #0x00000004
+
+	ldr r1, [r0]
+	mov r2, #0x00000007
+	bic r1, r1, r2, lsl #0x0f
+	orr r1, #0x00008000
+	str r1, [r0]
+
+	ldr r0, .gpclr1
+	mov r2, #0x00000080
+	mov r1, r2, lsl #0x08
+
+	ldr r1, [r0]
+
+	mov r1, #0x08
+	ldr r1, [r0]
+
+	b hang
+
+hang: b hang
+
+.reg:    .word 0x20200010
+.gpclr1: .word 0x2020002c
